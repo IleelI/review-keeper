aa379f7c2793f852db2f65e9b2b7d356f6878453
Bartosz Elert <elert.bartosz@gmail.com>
2024-04-03 16:58:32 +0200

    feat(review-keeper): added pagination

--- app/.server/data/reviews.ts
+++ app/.server/data/reviews.ts
@@ -1,43 +1,58 @@
 import type { PromiseReturnType } from "@prisma/client/extension";
 import { prisma } from "../service/db";
 
-export type ReviewForGrid = PromiseReturnType<typeof getReviewsForGrid>[number];
-export const getReviewsForGrid = async () => {
+export type ReviewForGrid = PromiseReturnType<
+  typeof getReviewsForGrid
+>["items"][0];
+
+export const getReviewsForGrid = async (page = 1, pageSize = 10) => {
   try {
-    const reviews = await prisma.review.findMany({
-      select: {
-        author: {
-          select: {
-            id: true,
-            username: true,
+    const [reviews, count] = await prisma.$transaction([
+      prisma.review.findMany({
+        select: {
+          _count: true,
+          author: {
+            select: {
+              id: true,
+              username: true,
+            },
           },
-        },
-        category: {
-          select: {
-            id: true,
-            name: true,
+          category: {
+            select: {
+              id: true,
+              name: true,
+            },
           },
+          createdAt: true,
+          id: true,
+          rating: true,
+          ratingScale: true,
+
+          title: true,
+          updatedAt: true,
         },
-        createdAt: true,
-        id: true,
-        rating: true,
-        ratingScale: true,
-        reactions: true,
-        title: true,
-        updatedAt: true,
-      },
-      orderBy: {
-        updatedAt: "desc",
-      },
-    });
-    return reviews.map(({ createdAt, reactions, updatedAt, ...review }) => ({
-      ...review,
-      reactionCount: reactions.length,
-      createdAt: createdAt.toISOString(),
-      updatedAt: updatedAt.toISOString(),
-    }));
+        skip: (page - 1) * pageSize,
+        take: pageSize,
+        orderBy: {
+          updatedAt: "desc",
+        },
+      }),
+      prisma.review.count(),
+    ]);
+
+    return {
+      items: reviews.map(({ createdAt, updatedAt, ...review }) => ({
+        ...review,
+        createdAt: createdAt.toISOString(),
+        updatedAt: updatedAt.toISOString(),
+      })),
+      totalItems: count,
+    };
   } catch (error) {
     console.error(error);
-    return [];
+    return {
+      items: [],
+      totalItems: 0,
+    };
   }
 };
--- app/routes/_index/components/ReviewCard/ReviewReactionCount.tsx
+++ app/routes/_index/components/ReviewCard/ReviewReactionCount.tsx
@@ -1,6 +1,6 @@
-import type { ReviewForGrid } from "~/.server/data/reviews";
-
-type ReviewReactionCountProps = Pick<ReviewForGrid, "reactionCount">;
+interface ReviewReactionCountProps {
+  reactionCount: number;
+}
 const ReviewReactionCount = ({ reactionCount }: ReviewReactionCountProps) => (
   <p className="text-sm text-neutral-600 dark:text-neutral-400">
     Reactions: <span className="font-medium">{reactionCount}</span>
